ac48e4f6c5dbc660be43a1b00a08f0649fc9671d
MrB-ForbesCTE <bradfordn@georgetownisd.org>
2026-01-16 09:13:34 -0600

    Create run.py

---
+++ run.py
@@ -0,0 +1,8 @@
+# In this file we will have our code to
+# run the Simple Adventure Game
+
+def main():
+    print("Hello World. This is my game. Test Balloon.")
+    
+if __name__ == "__main__":
+    main()
