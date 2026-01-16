6d868f20d34be9ca8c3bd8db66ada2f60d249681
MrB-ForbesCTE <bradfordn@georgetownisd.org>
2026-01-16 14:55:12 -0600

    Added simple healthbar logic

--- run.py
+++ run.py
@@ -4,7 +4,18 @@ import random
 import mechanics.dice as dice
 
 def main():
+    hp = 20
+    healthbar = "|"
     print(f"Let's get a random number: {dice.d20.roll()}")
+    for i in range(20):
+        if i < hp:
+            healthbar += "*"
+        else:
+            healthbar += "-"
+        if i == 19:
+            healthbar +="|"
+    print(healthbar)
+        
     
 if __name__ == "__main__":
     main()
